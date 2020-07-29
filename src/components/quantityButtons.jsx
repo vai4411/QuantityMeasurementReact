0debf51a94ee84b6814d10e588b5648b38e5b0fd
vaibhav <60617102+vai4411@users.noreply.github.com>
2020-07-29 22:46:52 +0530

    REFACTORED: used external service for load subunits api

--- src/components/quantityButtons.jsx
+++ src/components/quantityButtons.jsx
@@ -13,21 +13,15 @@ class QuantityButtons extends Component {
   }
 
   componentWillMount() {
-    const url1 = "http://localhost:8080/quantity/base_units";
-    const self = this;
-    const axios = require("axios");
-    axios({
-      method: "get",
-      url: url1,
-      headers: { "Content-Type": "application/json" },
-    })
-      .then(function (response) {
-        baseUnits = response.data.units;
-        self.setState({ data: baseUnits });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    this.loadUnits();
+  }
+
+  async loadUnits() {
+    const unit = await this.api.loadMainUnit();
+    console.log(unit);
+    this.setState({
+      data: unit,
+    });
   }
 
   changeSubunit = (id) => {
--- src/services/quantityApi.js
+++ src/services/quantityApi.js
@@ -1,41 +1,56 @@
-import React, { Component } from 'react'
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 const url = "http://localhost:8080/quantity/";
 
-class Api extends React.Component{
-	convert = (fromUnit,quantity,toUnit) => {
-		return axios({
-			method: "post",
-			url: url + "convert",
-			data: {
-			  firstSubUnit: fromUnit,
-			  quantity: Number(quantity),
-			  secondSubUnit: toUnit,
-			},
-			headers: { "Content-Type": "application/json" },
-		  })
-			.then(function (response) {
-			  console.log(response.data.result);
-			  return response.data.result;
-			})
-			.catch(function (error) {
-			  console.log(error);
-			});
-	}
-
-	loadUnit = (unit) => {
-    	return axios({
-        method: "get",
-        url: url + "sub_units/" + unit,
-        headers: { "Content-Type": "application/json" },
+class Api extends React.Component {
+  convert = (fromUnit, quantity, toUnit) => {
+    return axios({
+      method: "post",
+      url: url + "convert",
+      data: {
+        firstSubUnit: fromUnit,
+        quantity: Number(quantity),
+        secondSubUnit: toUnit,
+      },
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(function (response) {
+        console.log(response.data.result);
+        return response.data.result;
       })
-        .then(function (response) {
-		return response.data.units;
-        })
-        .catch(function (error) {
-          console.log(error);
-		});
-	}
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  loadUnit = (unit) => {
+    return axios({
+      method: "get",
+      url: url + "sub_units/" + unit,
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(function (response) {
+        return response.data.units;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  loadMainUnit = () => {
+    return axios({
+      method: "get",
+      url: url + "base_units",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then(function (response) {
+        console.log(response.data.units);
+        return response.data.units;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 }
 
 export default Api;
